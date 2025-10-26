d8b20ef2ca08ebd69fc2d783780f1773b9a7f5dd
Admin <Admin@Anandhi>
2025-10-26 22:53:07 +0530

    Assignments

--- assignments/src/com/tnsif/assignmentthree/Account.java
+++ assignments/src/com/tnsif/assignmentthree/Account.java
@@ -1,5 +1,22 @@
+
 package com.tnsif.assignmentthree;
 
-public class Account {
+public abstract class Account {
+    protected String accountHolder;
+    protected double balance;
 
+    public Account(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+    public abstract double getBalance();
+
+    public void displayAccountInfo() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Current Balance: " + balance);
+    }
 }
+
--- assignments/src/com/tnsif/assignmentthree/CheckingAccount.java
+++ assignments/src/com/tnsif/assignmentthree/CheckingAccount.java
@@ -1,5 +1,31 @@
 package com.tnsif.assignmentthree;
 
-public class CheckingAccount {
+public class CheckingAccount extends Account {
+    private double overdraftLimit;
 
+    public CheckingAccount(String accountHolder, double balance, double overdraftLimit) {
+        super(accountHolder, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited " + amount + " to Checking Account");
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= balance + overdraftLimit) {
+            balance -= amount;
+            System.out.println("Withdrew " + amount + " from Checking Account");
+        } else {
+            System.out.println("Withdrawal exceeds overdraft limit");
+        }
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
 }
